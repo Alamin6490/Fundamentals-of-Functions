e5de16b6b27f3d16b055a50037b75dd3ecb55715
Alamin6490 <132498750+Alamin6490@users.noreply.github.com>
2024-10-23 16:23:19 +0600

    node handle multiple parameters

---
+++ handle multiple parameters.js
@@ -0,0 +1,23 @@
+function vaatkhao(){
+    console.log('hand wash kore aso');
+    console.log('baso');
+    console.log('plate a khabar dhabar nao');
+    console.log('gopat gopat kore khao')
+
+}
+
+//vaatkhao();
+
+// 3 --> 3*3 = 9
+
+
+function square (number){
+    console.log(number);
+    console.log('value of the number parameter', number);
+    const borgo = number * number;
+    console.log('square of the given number is:', borgo);
+
+}
+
+square(5);
+console.log('--------');
