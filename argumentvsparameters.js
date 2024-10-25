5db69d4f8fd650f485b2f7ec533d960d4f3f9d34
Alamin6490 <132498750+Alamin6490@users.noreply.github.com>
2024-10-25 11:32:13 +0600

    argumentvsparameter

--- argumentvsparameters.js
+++ argumentvsparameters.js
@@ -4,3 +4,14 @@ function add(a, b) { // a এবং b হলো Parameters
   
   console.log(add(5, 3)); // 5 এবং 3 হলো Arguments
   // Output: 8
+
+  //
+
+  function difference (num1, num2){
+    const diff = num1- num2;
+    console.log(num1,num2, 'difference is', diff);
+  }
+  const fatherAge = 40;
+  const myAge = 10;
+
+  difference(fatherAge, myAge);
