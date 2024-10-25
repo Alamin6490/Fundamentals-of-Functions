a3858d7148a36cb12b5027ceecf36c47cf998b1a
Alamin6490 <132498750+Alamin6490@users.noreply.github.com>
2024-10-25 11:57:40 +0600

    argument va paramen

--- argumentvsparameters.js
+++ argumentvsparameters.js
@@ -7,6 +7,22 @@ function add(a, b) { // a এবং b হলো Parameters
 
   //
 
+  function doubleIt (number){
+    const doubled = number * 2;
+    console.log(number, doubled);
+  }
+console.log('I will call the function')
+doubleIt(15);
+console.log('--------');
+
+const number = 55;
+doubleIt(number);
+const money = 112;
+doubleIt(money);
+
+
+
+//
   function difference (num1, num2){
     const diff = num1- num2;
     console.log(num1,num2, 'difference is', diff);
