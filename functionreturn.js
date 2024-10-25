3ce40cc1daddbf193272eef805ae0668f269cdc0
Alamin6490 <132498750+Alamin6490@users.noreply.github.com>
2024-10-26 01:02:01 +0600

    Function return and set return value to a variable

---
+++ functionreturn.js
@@ -0,0 +1,19 @@
+function TenTime(number) {
+    const result = number * 10;
+    return result;
+
+}
+
+
+// another example
+function CutHalf(number) {
+    const half = number / 2;
+    return half;
+}
+
+TenTimes(5);
+
+const output = tenTimes(5);
+console.log('output',output);
+
+const bigNumber = tenTimes(99);
